5e9b1a6deacb08b2729386facba8ac317cc446f9
Neko Box Coder <nekoboxcoder@gmail.com>
2023-07-22 14:41:58 +0100

    Overhaul 3

--- Example.cpp
+++ Example.cpp
@@ -1,34 +1,36 @@
-#include "FunctionOverrides.hpp"
+#include "SimpleOverride.hpp"
 
-using namespace SimpleOverride;
-
-FunctionOverrides Overrider;
+SimpleOverride Overrider;
 
 int ChangeReturnValue(int a, float* b)
 {
-    //FO_RETURN_IF_FOUND(functionOverrideObj, functionRef, returnType, args...)
-    FO_RETURN_IF_FOUND(Overrider, ChangeReturnValue(int, float*), int, a, b);
+    //SO_RETURN_IF_FOUND(functionOverrideObj, functionRef, returnType, args...)
+    SO_RETURN_IF_FOUND(Overrider, ChangeReturnValue(int, float*), int, a, b);
     
     return 0;
 }
 
 void SetArgumentValue(int a, float& b, int* c)
 {
-    //FO_ARGUMENTS_IF_FOUND(functionOverrideObj, functionRef, args...)
-    FO_ARGUMENTS_IF_FOUND(Overrider, SetArgumentValue(int, float&, int*), a, b, c);
+    //SO_MODIFY_ARGUMENTS_IF_FOUND(overrideObj, functionRef, args...)
+    SO_MODIFY_ARGUMENTS_IF_FOUND(Overrider, SetArgumentValue(int, float&, int*), a, b, c);
     
-    //You can also use FO_ARGUMENTS_AND_RETURN_IF_FOUND to return a specify value
+    //You can also use SO_ARGUMENTS_AND_RETURN_IF_FOUND to return a specify value
     //  When the condition is met
 }
 
 int main()
 {
     //Example of overriding return value
-    Overrider   .OverrideReturns(ChangeReturnValue(int, float*))
-                .Returns(1)
-                .WhenCalledWith(2, 3.f)     //Pointers are automatically dereferenced when getting compared.
-                                            //      Unless it is cast to void*, then it won't be dereferenced.
-                .Times(2);                  //By default, it is infinite times if not specified
+    SO_OVERRIDE_RETURNS (Overrider, ChangeReturnValue(int, float*))  
+                        .Returns(1)
+                        
+                        //Pointers are automatically dereferenced when getting compared.
+                        //      Unless it is cast to void*, then it won't be dereferenced.
+                        .WhenCalledWith(2, 3.f)
+                        
+                        //By default, it is infinite times if not specified
+                        .Times(2);
 
     float testFloat = 3.f;
     assert(ChangeReturnValue(2, &testFloat) == 1 && ChangeReturnValue(2, &testFloat) == 1);
@@ -38,9 +40,12 @@ int main()
     
     
     //Example of overriding argument value
-    Overrider   .OverrideArgs(SetArgumentValue(int, float&, int*))
-                .SetArgs(FO_DONT_SET, 2.f, 3)   //Again, pointers are automatically dereferenced when getting set
-                .WhenCalledWith(4, FO_ANY, FO_ANY);
+
+    SO_OVERRIDE_ARGS(Overrider, SetArgumentValue(int, float&, int*))
+    
+                    //Again, pointers are automatically dereferenced when getting set
+                    .SetArgs(SO_DONT_SET, 2.f, 3)
+                    .WhenCalledWith(4, SO_ANY, SO_ANY);
 
     int testInt = 5;
     SetArgumentValue(4, testFloat, &testInt);
@@ -54,13 +59,13 @@ int main()
     
     //ReturnsByAction and SetArgsByAction can also be used to return or set arguments by lambda
     
-    //FO_DECLARE_INSTNACE(OverrideObjName) macro can be used inside a class declaration to 
+    //SO_DECLARE_INSTNACE(OverrideObjName) macro can be used inside a class declaration to 
     //  declare an instance of overriding object
 
-    //FO_DECLARE_OVERRIDE_METHODS(OverrideObjName) macro can be used inside a class declaration 
+    //SO_DECLARE_OVERRIDE_METHODS(OverrideObjName) macro can be used inside a class declaration 
     //  to declare proxy methods inline implementations to the override object
     
-    //FO_NonCopyable and FO_NonComparable can be used for objects that are not copyable or comparable.
+    //SO_NonCopyable and SO_NonComparable can be used for objects that are not copyable or comparable.
     //  These objects won't be compared against in WhenCalledWith.
     
     //You also have access to "If" clause for additonal conditions
@@ -68,7 +73,7 @@ int main()
     //Similarly, you have "WhenCalledExpectedly_Do" and "Otherwise_Do" clauses for performing custom actions
     //  when the condition is matched or not respectively
     
-    //For usage on these extra actions, see Tests/FunctionOverridesTests.cpp
+    //For usage on these extra actions, see Tests/SimpleOverrideTests.cpp
     
     return 0;
 }
